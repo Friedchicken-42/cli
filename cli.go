45973fb32556d1d8830e841b338921dea1472395
Friedchicken <paololego20+github@gmail.com>
2022-03-23 16:32:07 +0100

    added flags support

--- cli.go
+++ cli.go
@@ -1,89 +1,197 @@
 ﻿package cli
 
-import "errors"
+import (
+	"errors"
+)
 
-type Context map[string]string
+type Context struct {
+	Args  map[string]string
+	Flags map[string]bool
+}
 
 type Args []string
 
+type Option struct {
+	Name   string
+	Prompt string
+	Short  rune
+	IsFlag bool
+}
+
+type Options []*Option
+
 type Command struct {
-    Name string
-    Commands Commands
-    Arguments Args
-    Context *Context
-    Action func(c *Context) error
+	Name      string
+	Commands  Commands
+	Arguments Args
+	Options   Options
+	Context   *Context
+	Action    func(c *Context) error
 }
 
 type Commands []*Command
 
 type App Command
 
-func (c *Context) Set(key, value string) {
-    (*c)[key] = value
-}
+func (c *Context) Get(name string) (string, bool) {
+    if v, ok := c.Args[name]; ok {
+        return v, ok
+    }
 
-func (c *Context) Get(key string) string {
-    return (*c)[key]
+    if ok := c.Flags[name]; ok {
+        return "", ok
+    }
+
+    return "", false
 }
 
 func (c *Command) ShareContext(context *Context) {
-    c.Context = context
-    for i := range c.Commands {
-        c.Commands[i].ShareContext(context)
-    }
+	c.Context = context
+	for i := range c.Commands {
+		c.Commands[i].ShareContext(context)
+	}
 }
 
-func (c Command) SetArgs(args []string) {
-    for i := range args {
-        c.Context.Set(c.Arguments[i], args[i])
-    }
+func (c *Command) SetArg(key, value string) {
+	c.Context.Args[key] = value
 }
 
-func (c Commands) Get(name string) (*Command, error) {
-    for i := range c {
-        if c[i].Name == name {
-            return c[i], nil
-        }
-    }
-    return nil, errors.New("command not found: " + name)
+func (c *Command) FindOptionPrompt(name string) *Option {
+	for i := range c.Options {
+		if c.Options[i].Prompt == name {
+			return c.Options[i]
+		}
+	}
+	return nil
 }
 
-func (c Commands) Search(args []string) (*Command, error) {
-    command, err := c.Get(args[0])
-    if err != nil {
-        return nil, err
+func (c *Command) FindOptionShort(name rune) *Option {
+	for i := range c.Options {
+		if c.Options[i].Short == name {
+			return c.Options[i]
+		}
+	}
+	return nil
+}
+
+func (c *Command) GetOption(name string) *Option {
+	if name[1] == '-' {
+		return c.FindOptionPrompt(name[2:])
+	}
+	return c.FindOptionShort(rune(name[1]))
+}
+
+func (c *Command) SetOption(name, value string) {
+	c.Context.Args[name] = value
+}
+
+func (c *Command) SetFlag(name string) {
+	c.Context.Flags[name] = true
+}
+
+func (c *Command) SetArgs(args []string) error {
+	a := 0
+
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+
+		if arg[0] == '-' {
+			option := c.GetOption(arg)
+
+			if option == nil {
+				return errors.New("missing option: " + arg)
+			}
+
+			var name string
+			if option.Short != 0 {
+				name = string(option.Short)
+			}
+			if option.Prompt != "" {
+				name = option.Prompt
+			}
+			if option.Name != "" {
+				name = option.Name
+			}
+
+			if option.IsFlag == false {
+				i++
+				value := args[i]
+
+				c.SetOption(name, value)
+			} else {
+				c.SetFlag(name)
+			}
+
+		} else {
+			c.SetArg(c.Arguments[a], arg)
+			a++
+		}
+	}
+
+	if len(c.Arguments) != a {
+		return errors.New("mismatcing arguments")
+	}
+
+	return nil
+}
+
+func (c Command) Get(name string) (*Command, error) {
+	for i := range c.Commands {
+		if c.Commands[i].Name == name {
+			return c.Commands[i], nil
+		}
+	}
+	return nil, errors.New("command not found: " + name)
+}
+
+func (c *Command) Search(args []string) (*Command, []string, error) {
+    if len(args) == 0 {
+        return c, nil, nil
     }
 
-    if len(command.Commands) == 0 {
-        command.SetArgs(args[1:])
-        return command, nil
-    }
+	command, err := c.Get(args[0])
+	if err == nil {
+		return command.Search(args[1:])
+	}
 
-    return command.Commands.Search(args[1:])
+	return c, args, nil
 }
 
 func (a *App) shareContext() {
-    context := &Context{}
-    a.Context = context
+	context := &Context{}
+	context.Args = make(map[string]string)
+	context.Flags = make(map[string]bool)
 
-    for i := range a.Commands {
-        a.Commands[i].ShareContext(context)
-    }
+	a.Context = context
+
+	for i := range a.Commands {
+		a.Commands[i].ShareContext(context)
+	}
+}
+
+func (a *App) Search(args []string) (*Command, []string, error) {
+	c := Command(*a)
+	command := &c
+
+	return command.Search(args)
 }
 
 func (a *App) Run(args []string) error {
-    a.shareContext()
+	a.shareContext()
 
-    command, err := a.Commands.Search(args[1:])
-    if err != nil {
-        return err
-    }
+	command, args, err := a.Search(args[1:])
+	if err != nil {
+		return err
+	}
 
-    if command.Action == nil {
-        return errors.New("missing action for command " + command.Name)
-    }
+	err = command.SetArgs(args)
+	if err != nil {
+		return err
+	}
 
-    command.Action(command.Context)
+	if command.Action == nil {
+		return errors.New("missing action for command " + command.Name)
+	}
 
-    return nil
+	return command.Action(command.Context)
 }
--- cli_test.go
+++ cli_test.go
@@ -1,34 +1,92 @@
 ﻿package cli
 
 import (
-	"log"
+	"errors"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	app := App{
+func CreateApp() *App {
+	app := &App{
 		Commands: Commands{
 			&Command{
-				Name: "mod",
-				Commands: Commands{
-					&Command{
-						Name:      "init",
-						Arguments: Args{"name"},
-						Action: func(c *Context) error {
-							log.Println("go mod init " + c.Get("name"))
-							return nil
-						},
+				Name: "init",
+				Action: func(c *Context) error {
+					return nil
+				},
+			},
+			&Command{
+				Name:      "clone",
+				Arguments: Args{"url"},
+				Options: Options{
+					&Option{
+						Short:  'n',
+						Prompt: "name",
+						Name:   "NAME",
 					},
+				},
+				Action: func(c *Context) error {
+					v, ok := c.Get("NAME")
+
+					if !ok {
+						return errors.New("missing option NAME")
+					}
+					if v != "test" {
+						return errors.New("wrong name")
+					}
+					return nil
 				},
 			},
 		},
+		Options: Options{
+			&Option{
+				Short:  'v',
+				IsFlag: true,
+			},
+		},
+		Action: func(c *Context) error {
+			if _, ok := c.Get("v"); !ok {
+				return errors.New("missing flag version")
+			}
+			return nil
+		},
 	}
 
-	args := []string{"go", "mod", "init", "kek"}
-	err := app.Run(args)
-	log.Println(app.Context)
+	return app
+}
 
+func TestGeneric(t *testing.T) {
+	app := CreateApp()
+
+	err := app.Run([]string{"git", "clone", "--name", "test", "kek"})
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
+	}
+}
+
+func TestBase(t *testing.T) {
+	app := CreateApp()
+
+	err := app.Run([]string{"git", "init"})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMissing(t *testing.T) {
+	app := CreateApp()
+
+	err := app.Run([]string{"git", "clone"})
+	if err == nil {
+		t.Fatal("this should have failed")
+	}
+
+}
+
+func TestGlobalFlags(t *testing.T) {
+	app := CreateApp()
+
+	err := app.Run([]string{"git", "-v"})
+	if err != nil {
+		t.Fatal(err)
 	}
 }
